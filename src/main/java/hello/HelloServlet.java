c537b689c4037857676caf579eafbbb26c708f1d
Rafael Lins Pinheiro <rafaellinsp@gmail.com>
2022-04-17 15:41:13 -0300

    Hora local

--- src/main/java/hello/HelloServlet.java
+++ src/main/java/hello/HelloServlet.java
@@ -167,74 +167,179 @@ public class HelloServlet extends HttpServlet {
         
         msg = msg+nome+", ";
         
-         ZoneId zone1 = ZoneId.of("GMT+9");
-        LocalTime Tempo = LocalTime.now();
-        LocalTime now2 = LocalTime.now(zone1);
-        if(Tempo.getHour() >=0 && Tempo.getHour() <= 12){
-        switch(lang){
-            case "pt":
-                msg = msg + "Bom dia !";
-                break;
-            case "en":
-                msg = msg + "Good morning !";
-                break;
-            case "fr":
-                msg = msg + "Bon matin !";
-                break;
-            case "de":
-                msg = msg + "Guten morgen !";
-                break;
-            case "es":
-                msg = msg + "Buen día !";
-                break;
-            case "jp":
-                msg = msg + "Ohayō !";
-                break;
+        
+        String hora = request.getParameter("hora");
+        if(hora==null)
+            hora = "n";
+        
+        
+        switch(hora){
+            case "n":
+                LocalTime Tempo = LocalTime.now();
+                if(Tempo.getHour() >=0 && Tempo.getHour() <= 12){
+                    switch(lang){
+                        case "pt":
+                            msg = msg + "Bom dia !";
+                            break;
+                        case "en":
+                            msg = msg + "Good morning !";
+                            break;
+                        case "fr":
+                            msg = msg + "Bon matin !";
+                            break;
+                        case "de":
+                            msg = msg + "Guten morgen !";
+                            break;
+                        case "es":
+                            msg = msg + "Buen día !";
+                            break;
+                        case "jp":
+                            msg = msg + "Ohayō !";
+                            break;
+                    }
+                }else if (Tempo.getHour() > 12 && Tempo.getHour() <= 18) {
+                        switch(lang){
+                            case "pt":
+                                msg = msg + "Boa tarde !";
+                                break;
+                            case "en":
+                                msg = msg + "Good afternoon !";
+                                break;
+                            case "fr":
+                                msg = msg + "Bon après-midi !";
+                                break;
+                            case "de":
+                                msg = msg + "Guten tag !";
+                                break;
+                            case "es":
+                                msg = msg + "Buenas tardes !";
+                                break;
+                            case "jp":
+                                msg = msg + "Kon'nichiwa !";
+                                break;
+                }
+                } else {
+                        switch(lang){
+                            case "pt":
+                            msg = msg + "Boa noite !";
+                            break;
+                        case "en":
+                            msg = msg + "Good night !";
+                            break;
+                        case "fr":
+                            msg = msg + "Bonne nuit !";
+                            break;
+                        case "de":
+                            msg = msg + "Gute nacht !";
+                            break;
+                        case "es":
+                            msg = msg + "Buenas noches !";
+                            break;
+                        case "jp":
+                            msg = msg + "Oyasuminasai !";
+                            break;
+                }
         }
-        }else if (Tempo.getHour() > 12 && Tempo.getHour() <= 18) {
-            switch(lang){
-                case "pt":
-                    msg = msg + "Boa tarde !";
-                    msg = msg + now2;
+                break;
+                
+            case "s":
+                LocalTime TempoLocal = LocalTime.now();
+                ZoneId zone1 = ZoneId.of("GMT+9");
+                
+                switch(lang){
+                    case "pt":
+                    TempoLocal = LocalTime.now();
                     break;
-                case "en":
-                    msg = msg + "Good afternoon !";
-                    break;
-                case "fr":
-                    msg = msg + "Bon après-midi !";
-                    break;
-                case "de":
-                    msg = msg + "Guten tag !";
-                    break;
-                case "es":
-                    msg = msg + "Buenas tardes !";
-                    break;
-                case "jp":
-                    msg = msg + "Kon'nichiwa !";
-                    break;
-            }
-        } else {
-            switch(lang){
-                case "pt":
-                    msg = msg + "Boa noite !";
-                    break;
-                case "en":
-                    msg = msg + "Good night !";
-                    break;
-                case "fr":
-                    msg = msg + "Bonne nuit !";
-                    break;
-                case "de":
-                    msg = msg + "Gute nacht !";
-                    break;
-                case "es":
-                    msg = msg + "Buenas noches !";
-                    break;
-                case "jp":
-                    msg = msg + "Oyasuminasai !";
-                    break;
-            }
+                    case "en":
+                        zone1 = ZoneId.of("GMT-4");
+                        TempoLocal = LocalTime.now(zone1);
+                        break;
+                    case "fr":
+                        zone1 = ZoneId.of("GMT+2");
+                        TempoLocal = LocalTime.now(zone1);
+                        break;
+                    case "de":
+                        zone1 = ZoneId.of("GMT+2");
+                        TempoLocal = LocalTime.now(zone1);
+                        break;
+                    case "es":
+                        zone1 = ZoneId.of("GMT+2");
+                        TempoLocal = LocalTime.now(zone1);
+                        break;
+                    case "jp":
+                        zone1 = ZoneId.of("GMT+9");
+                        TempoLocal = LocalTime.now(zone1);
+                        break;
         }
+                        
+                        
+                if(TempoLocal.getHour() >=0 && TempoLocal.getHour() <= 12){
+                    switch(lang){
+                        case "pt":
+                            msg = msg + "Bom dia ! Sua hora local:" + TempoLocal;
+                            break;
+                        case "en":
+                            msg = msg + "Good morning ! Your local time:" + TempoLocal;
+                            break;
+                        case "fr":
+                            msg = msg + "Bon matin ! Votre heure locale:" + TempoLocal;
+                            break;
+                        case "de":
+                            msg = msg + "Guten morgen ! Ihre Ortszeit:" + TempoLocal;
+                            break;
+                        case "es":
+                            msg = msg + "Buen día ! Tu hora local:" + TempoLocal;
+                            break;
+                        case "jp":
+                            msg = msg + "Ohayō ! Anata no genchi jikan:" + TempoLocal;
+                            break;
+                    }
+                }else if (TempoLocal.getHour() > 12 && TempoLocal.getHour() <= 18) {
+                    switch(lang){
+                        case "pt":
+                            msg = msg + "Boa tarde ! Sua hora local:" + TempoLocal;
+                            break;
+                        case "en":
+                            msg = msg + "Good afternoon ! Your local time:" + TempoLocal;
+                            break;
+                        case "fr":
+                            msg = msg + "Bon après-midi ! Votre heure locale:" + TempoLocal;
+                            break;
+                        case "de":
+                            msg = msg + "Guten tag ! Ihre Ortszeit:" + TempoLocal;
+                            break;
+                        case "es":
+                            msg = msg + "Buenas tardes ! Tu hora local:" + TempoLocal;
+                            break;
+                        case "jp":
+                            msg = msg + "Kon'nichiwa ! Anata no genchi jikan:" + TempoLocal;
+                            break;
+                    }
+                } else {
+                    switch(lang){
+                        case "pt":
+                            msg = msg + "Boa noite ! Sua hora local:" + TempoLocal;
+                            break;
+                        case "en":
+                            msg = msg + "Good night ! Your local time:" + TempoLocal;
+                            break;
+                        case "fr":
+                            msg = msg + "Bonne nuit ! Votre heure locale:" + TempoLocal;
+                            break;
+                        case "de":
+                            msg = msg + "Gute nacht ! Ihre Ortszeit:" + TempoLocal;
+                            break;
+                        case "es":
+                            msg = msg + "Buenas noches ! Tu hora local:" + TempoLocal;
+                            break;
+                        case "jp":
+                            msg = msg + "Oyasuminasai ! Anata no genchi jikan:" + TempoLocal;
+                            break;
+                    }
+                }
+                break;
+            }
+         
         
 
 
